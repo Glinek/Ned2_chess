ba1e5c5f88a15497199096825860d0c298687b86
Glinek <szymonglinka2006@gmail.com>
2024-12-04 18:28:15 +0100

    new file:   .idea/.gitignore 	new file:   .idea/FirstChess.iml 	new file:   .idea/inspectionProfiles/Project_Default.xml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   Boards_diff.py 	new file:   board.jpg 	new file:   board.py 	new file:   board_poses4.txt 	new file:   chessboard_corners.py 	new file:   count_board_picks.py 	new file:   grab_board.py 	new file:   grab_poses.py 	new file:   home.py 	new file:   main.py 	new file:   pick_poses_test.py 	new file:   robot_move.py 	new file:   threshold_tune.py 	new file:   tool_move.py 	deleted:    .idea/.gitignore 	deleted:    .idea/FirstChess.iml 	deleted:    .idea/inspectionProfiles/Project_Default.xml 	deleted:    .idea/inspectionProfiles/profiles_settings.xml 	deleted:    .idea/misc.xml 	deleted:    .idea/modules.xml 	deleted:    .idea/vcs.xml 	modified:   Boards_diff.py 	deleted:    board.jpg 	modified:   board.py 	deleted:    board_poses4.txt 	modified:   chessboard_corners.py 	modified:   count_board_picks.py 	modified:   grab_board.py 	modified:   grab_poses.py 	modified:   home.py 	modified:   main.py 	modified:   pick_poses_test.py 	modified:   robot_move.py 	modified:   threshold_tune.py 	modified:   tool_move.py

---
+++ board.py
@@ -0,0 +1,38 @@
+# import numpy as np
+import cv2
+from os import environ
+
+def suppress_qt_warnings():
+    environ["QT_DEVICE_PIXEL_RATIO"] = "0"
+    environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    environ["QT_SCREEN_SCALE_FACTORS"] = "1"
+    environ["QT_SCALE_FACTOR"] = "1"
+
+suppress_qt_warnings()
+
+img = cv2.imread("ch3.webp")
+
+img = cv2.resize(img, (800, 800))
+
+cv2.imshow('Original', img)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Gray scale', gray)
+
+ret, thresh = cv2.threshold(gray, 150, 255, 0)
+canny = cv2.Canny(thresh, 30, 200)
+cv2.imshow("Threshold", thresh)
+contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+print("Number of Contours = " ,len(contours))
+for l in contours:
+    print(l)
+cv2.imshow('Canny Edges', canny)
+
+cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
+cv2.imshow('Contours', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
